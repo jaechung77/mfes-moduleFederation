2815fc622afdad0f1e8274e1ef564aa10984413d
Jae Chung <jaechung77@gmail.com>
2024-09-14 23:03:43 +1000

    fix typo

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'margeting',
+      name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap',
